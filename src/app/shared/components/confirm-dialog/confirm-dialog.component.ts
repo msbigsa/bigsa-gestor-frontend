2a8378cc8aed95e2a985472aab5fdb55929168e3
BIGSA\ruben <msbigsa@gmail.com>
2026-07-31 12:55:25 -0400

    refactor modal confirm/alert

--- src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
+++ src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -1,50 +1,95 @@
 import { Component, Inject } from '@angular/core';
 import {
   MAT_DIALOG_DATA,
-  MatDialogRef
+  MatDialogRef,
+  MatDialogModule
 } from '@angular/material/dialog';
 
-import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import { ConfirmDialogResult } from './confirm-dialog-result.enum';
+import { MatIconModule } from '@angular/material/icon';
 
+import { ConfirmDialogResult } from './confirm-dialog-result.enum';
 
 export interface ConfirmDialogData {
   title: string;
-  message: string;  
-  cancelText: string;
-  confirmText: string;
+  message: string;
 
+  cancelText?: string;
+  confirmText?: string;
   confirmTextAdic?: string;
-  useConfirmTextAdic?: boolean;
-}
 
+  type?: 'danger' | 'warning' | 'info' | 'success' | 'help';
+}
 
 @Component({
   selector: 'app-confirm-dialog',
-  standalone: true,
   imports: [
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
-  templateUrl: './confirm-dialog.component.html'
+  templateUrl: './confirm-dialog.component.html',
+  styleUrl: './confirm-dialog.component.scss'
 })
 export class ConfirmDialogComponent {
 
   constructor(
-    private dialogRef: MatDialogRef<ConfirmDialogComponent,ConfirmDialogResult>,
+    private dialogRef: MatDialogRef<ConfirmDialogComponent, ConfirmDialogResult>,
     @Inject(MAT_DIALOG_DATA) public data: ConfirmDialogData
   ) {}
 
+  get icon(): string {
+    switch (this.data.type) {
+      case 'danger':
+        return 'delete';
+
+      case 'warning':
+        return 'warning';
+
+      case 'success':
+        return 'check_circle_outline';
+
+      case 'info':
+        return 'info_outline';
+
+      case 'help':
+      default:
+        return 'help_outline';          
+    }
+  }
+
+  get iconColor(): 'primary' | 'warn' {
+    switch (this.data.type) {
+      case 'danger':
+      case 'warning':
+        return 'warn';
+
+      default:
+        return 'primary';
+    }
+  }
+
+  get buttonColor(): 'primary' | 'warn' {
+    return this.data.type === 'danger'
+      ? 'warn'
+      : 'primary';
+  }
+
   confirmarAdicional(): void {
-  this.dialogRef.close(ConfirmDialogResult.CONFIRM_ADDITIONAL);
-}
+    this.dialogRef.close(
+      ConfirmDialogResult.CONFIRM_ADDITIONAL
+    );
+  }
 
-confirmar(): void {
-  this.dialogRef.close(ConfirmDialogResult.CONFIRM);
-}
+  confirmar(): void {
+    this.dialogRef.close(
+      ConfirmDialogResult.CONFIRM
+    );
+  }
 
-cancelar(): void {
-  this.dialogRef.close(ConfirmDialogResult.CANCEL);
-}
+  cancelar(): void {
+    this.dialogRef.close(
+      ConfirmDialogResult.CANCEL
+    );
+  }
 }
